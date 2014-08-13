079f714fa3de2475138c5f66877dcefacb6738ae
Fynn <no@email.kr>
2014-08-13 20:11:53 +0200

    init main.cc

---
+++ build/CMakeFiles/qpsing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qpsing.dir/src/main.cc.o"
+  "qpsing.pdb"
+  "qpsing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qpsing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
